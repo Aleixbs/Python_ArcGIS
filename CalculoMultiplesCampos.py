91ca3eac4695b3474d6788e12873d12fb7bece20
Aleix Batlle <84009394+Aleixbs@users.noreply.github.com>
2024-02-07 09:26:32 +0100

    Update CalculoMultiplesCampos.py

--- CalculoMultiplesCampos.py
+++ CalculoMultiplesCampos.py
@@ -15,37 +15,6 @@ def calc():
 # Specify the path to your feature class
 feature_class = r"C:\carpeta\NombreGDB.gdb\NombreFC"
 
-# Get a list of field names in the feature class
-field_names = [field.name for field in arcpy.ListFields(feature_class)]
-
-# Iterate over the fields and update them if they don't have values
-with arcpy.da.UpdateCursor(feature_class, field_names) as cursor:
-    for row in cursor:
-        for i, field_value in enumerate(row):
-            if field_value is None:
-                # Apply the calc() function to fill in the None values
-                row[i] = calc()
-        cursor.updateRow(row)
-Traceback (most recent call last):
-  File "<string>", line 28, in <module>
-RuntimeError: The value type is incompatible with the field type. [ObjectID_1]
-import arcpy
-import random
-
-# Define your calc() function
-def calc():
-    sampleList = [1, 2]
-    randomList = random.choices(sampleList, weights=(9, 1), k=1)
-    if randomList[0] == 1:
-        return 'No'
-    elif randomList[0] == 2:
-        return 'Si'
-    else:
-        return 'No'
-
-# Specify the path to your feature class
-feature_class = r"C:\carpeta\NombreGDB.gdb\NombreFC"
-
 # Get a list of field names and their data types in the feature class
 field_info = [(field.name, field.type) for field in arcpy.ListFields(feature_class)]
 
